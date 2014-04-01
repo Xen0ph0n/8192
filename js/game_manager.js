2ceb06b2ca7fb4c5b55aac169964903155d0e421
root <root@ip-10-196-125-44.ec2.internal>
2014-04-01 23:41:08 +0000

    Changed to 5x5 8192 to win with 8 blocks spawning 5% of the time

--- js/game_manager.js
+++ js/game_manager.js
@@ -72,7 +72,16 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var randomnum = Math.random()
+    if (randomnum > 0.95){
+      var value = 8
+      }
+    else if (randomnum > 0.80){
+      var value = 4
+      }
+    else {
+      var value = 2
+      } 
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -171,7 +180,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          if (merged.value === 8192) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
